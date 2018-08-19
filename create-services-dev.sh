96dccfffdd1f73d1f2854d009af9c8d627034366
Doug Saus <ds3161@Dougs-MacBook-Pro.local>
2018-08-19 16:34:20 -0500

    Added services script

---
+++ create-services-dev.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cf login -a api.run.pivotal.io -o mdidb-dev -s development
+cf create-service p-circuit-breaker-dashboard trial circuit-breaker
+cf create-service p-service-registry trial service-registry -c '{"count": 1}'
+cf create-service p-config-server trial config-server -c '{"count": 1, "git": { "uri": "https://github.com/Delmonte3161/mdidb-config" }}'
+cf create-service elephantsql turtle services-db
+cf create-service elephantsql turtle geo-db
+cf target -s development
