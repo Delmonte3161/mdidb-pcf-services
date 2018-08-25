d35dec01092609c8ceb90678983a1556d965e5f5
Doug Saus <doug.saus@yahoo.com>
2018-08-25 17:03:54 -0500

    Updated services

--- create-services-dev.sh
+++ create-services-dev.sh
@@ -5,4 +5,6 @@ cf create-service p-service-registry trial service-registry -c '{"count": 1}'
 cf create-service p-config-server trial config-server -c '{"count": 1, "git": { "uri": "https://github.com/Delmonte3161/mdidb-config" }}'
 cf create-service elephantsql turtle services-db
 cf create-service elephantsql turtle geo-db
+cf create-service elephantsql turtle auth-db
+cf create-service cloudamqp lemur rabbit
 cf target -s development
